e97dc1567ae114117e887d303377fcba07382d8b
unknown <wls4370@gmail.com>
2022-07-12 18:52:04 +0900

    feat:MIME 메시지로 변경. 가짜 인증번호 설정

--- src/main/java/com/todoay/api/domain/mailSender/controller/MailSendingController.java
+++ src/main/java/com/todoay/api/domain/mailSender/controller/MailSendingController.java
@@ -3,16 +3,16 @@ package com.todoay.api.domain.mailSender.controller;
 import com.todoay.api.domain.mailSender.dto.MailDto;
 import com.todoay.api.domain.mailSender.service.MailSendingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MailSendingController {
     private final MailSendingService mailSendingService;
 
     @GetMapping("/mail")
-    public void sendMail(MailDto mailDto) {
-        mailSendingService.sendEmail(mailDto);
+    public void sendMail(String email) {
+        mailSendingService.sendEmail(email);
     }
 }
--- src/main/java/com/todoay/api/domain/mailSender/service/MailSendingService.java
+++ src/main/java/com/todoay/api/domain/mailSender/service/MailSendingService.java
@@ -1,21 +1,31 @@
 package com.todoay.api.domain.mailSender.service;
 
 import com.todoay.api.domain.mailSender.dto.MailDto;
+import com.todoay.api.domain.mailSender.utility.MailHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 @Service
 @RequiredArgsConstructor
 public class MailSendingService {
     private final JavaMailSender mailSender;
-    public void sendEmail(MailDto mailDto) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mailDto.getAddress());
-        mailMessage.setSubject(mailDto.getTitle());
-        mailMessage.setText(mailDto.getMessage());
-        mailSender.send(mailMessage);
+
+    public void sendEmail(String email) {
+        try {
+            MailHandler mailHandler = new MailHandler(mailSender);
+            mailHandler.setTo(email);
+            mailHandler.setSubject("[TODOAY] 이메일 인증을 완료해주세요.");
+            String emailToken = "1234";
+            // emailToken = TokenManager.createEmailToken(email, 60*5);
+            mailHandler.setText("<h1>TODOAY</h1></br><p>CODE: " + emailToken +"</p></br>", true);
+            mailHandler.send();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
