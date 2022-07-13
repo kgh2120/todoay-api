b2fd53d6e66cf952d39f8bf78ca3136281d7c4d3
kyoohyun <kgh2120@naver.com>
2022-07-14 00:20:22 +0900

    feat : MethodArgumentNotValidException 처리 및 반환 객체 생성
    - MethodArgumentNotValidException는 @Validated, @Valid에 의해서 설정된 양식에 맞지 않는 값들에서 터지는 예외 클래스이다.
    - 해당 예외가 터질때 반환값을 넣어주는 ValidErrorResponse를 생성하고, 유효성 검사에서 실패한 경우를 담은 ValidDetail class를 제작하였다.

---
+++ src/main/java/com/todoay/api/global/exception/ValidErrorResponse.java
@@ -0,0 +1,44 @@
+package com.todoay.api.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidErrorResponse implements ErrorCode{
+
+    private final HttpStatus httpStatus;
+    private final String name;
+    private final String detailMessage;
+    private final LocalDateTime timeStamp = LocalDateTime.now();
+    private final List<ValidDetail> details = new ArrayList<>();
+
+
+    public ValidErrorResponse() {
+        httpStatus = HttpStatus.BAD_REQUEST;
+        name = "ARGUMENT_FORMAT_INVALID";
+        detailMessage = "양식에 맞지 않은 입력값이 입력되었습니다.";
+    }
+
+    public void addDetail(ValidDetail detail) {
+        this.details.add(detail);
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public String getDetailMessage() {
+        return detailMessage;
+    }
+}
