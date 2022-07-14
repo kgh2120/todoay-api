e8e5eb73c4e042404ee6c867d1df58ce5ecc6167
qkrdmswl <eunji303@naver.com>
2022-07-14 16:03:35 +0900

    Auth Entity, Profile Entity 생성

--- src/main/java/com/todoay/api/domain/auth/entity/Auth.java
+++ src/main/java/com/todoay/api/domain/auth/entity/Auth.java
@@ -1,4 +1,22 @@
 package com.todoay.api.domain.auth.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Auth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO, TABLE, SEQUENCE
+    @Column(name = "auth_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
 }
--- src/main/java/com/todoay/api/domain/profile/entity/Profile.java
+++ src/main/java/com/todoay/api/domain/profile/entity/Profile.java
@@ -1,4 +1,31 @@
 package com.todoay.api.domain.profile.entity;
+import com.todoay.api.domain.auth.entity.Auth;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Profile {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "profile_id")
+    private Long id;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "img_url")
+    private String img_url;
+
+    @Column(name = "intro_msg")
+    private String intro_msg;
+
+    // Auth table의 PK 참조 (FK)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auth_id")
+    private Auth auth;
 }
