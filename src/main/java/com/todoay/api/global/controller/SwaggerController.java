15aaec46b33f73242e20002ba20ef8fd4dc30e85
kyoohyun <kgh2120@naver.com>
2022-07-16 11:17:28 +0900

    chore : Swagger 사용 예시 추가

--- src/main/java/com/todoay/api/global/controller/SwaggerController.java
+++ src/main/java/com/todoay/api/global/controller/SwaggerController.java
@@ -1,10 +1,10 @@
 package com.todoay.api.global.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
-@Slf4j
+@ApiIgnore
 @Controller
 public class SwaggerController {
 
