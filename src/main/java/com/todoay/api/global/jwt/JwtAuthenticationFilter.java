1c9624bc9734cbe9a8f1269be427b8d578e6766d
qkrdmswl <eunji303@naver.com>
2022-07-20 17:42:08 +0900

    JwtAuthenticationFilter class 구현

---
+++ src/main/java/com/todoay/api/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package com.todoay.api.global.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// JWT 토큰으로 인증하고 SecurityContextHolder에 추가하는 필터를 설정하는 클래스
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(servletRequest);  // (1)
+        LOGGER.info("[doFilterInternal] token 값 추출 완료. token : {}", token);
+
+        LOGGER.info("[doFilterInternal] token 값 유효성 체크 시작");
+        if (token != null && jwtTokenProvider.validateToken(token)) {  // (2)
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);  // (3)
+            LOGGER.info("[doFilterInternal] token 값 유효성 체크 완료");
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
+// (1) JwtTokenProvider를 통해 servletRequest에서 토큰 추출
+// (2) 토큰에 대한 유효성 검사
+// (3) 토큰이 유효하다면 Authentication 객체를 생성해서 SecurityContextHolder에 추가
