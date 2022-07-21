7ba85bdd631d5259c6b79fb90550e18d2277a186
unknown <wls4370@gmail.com>
2022-07-21 20:29:40 +0900

    docs:누락된 swagger docs 추가. api version 지정

--- src/main/java/com/todoay/api/ApiApplication.java
+++ src/main/java/com/todoay/api/ApiApplication.java
@@ -1,6 +1,8 @@
 package com.todoay.api;
 
 import com.todoay.api.domain.auth.Dto.AuthSaveDto;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@OpenAPIDefinition(info = @Info(title = "TODOAY Open API", version = "${springdoc.version}"))
 public class ApiApplication {
 
 	public static void main(String[] args) {
