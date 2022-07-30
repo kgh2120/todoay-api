8bf6ee4ce86990104ebfd5583bdabc3d71326944
kyoohyun <kgh2120@naver.com>
2022-07-30 15:06:37 +0900

    chore : GlobalExceptionHandler 중복 코드 제거
    #87

--- src/main/java/com/todoay/api/global/config/GlobalExceptionHandler.java
+++ src/main/java/com/todoay/api/global/config/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.todoay.api.global.config;
 
-import com.todoay.api.global.exception.*;
+import com.todoay.api.global.exception.AbstractApiException;
+import com.todoay.api.global.exception.ErrorResponse;
+import com.todoay.api.global.exception.GlobalErrorCode;
+import com.todoay.api.global.exception.ValidErrorResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
@@ -8,15 +11,13 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.List;
-import java.util.stream.Collectors;
 
+import static com.todoay.api.global.exception.GlobalErrorCode.*;
 import static com.todoay.api.global.exception.GlobalErrorCode.SQL_INTEGRITY_CONSTRAINT_VIOLATION;
 
 @Slf4j
@@ -29,20 +30,6 @@ public class GlobalExceptionHandler {
         return ErrorResponse.toResponseEntity(e, request.getRequestURI());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request) {
-        List<ValidDetail> details = ex.getBindingResult().getFieldErrors().stream().map(e ->
-                ValidDetail.builder()
-                        .code(e.getCode())
-                        .field(e.getField())
-                        .rejectedValue(e.getRejectedValue())
-                        .message(e.getDefaultMessage())
-                        .build()
-        ).collect(Collectors.toList());
-        return ValidErrorResponse.toResponseEntity(details, request.getRequestURI());
-    }
-
-    // GET params 는 MethodArgumentNoValidException 에 걸리지 않고 그 super class 인 BindException 에 걸림
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ValidErrorResponse> handleBindException(BindException ex, HttpServletRequest request) {
         return ValidErrorResponse.toResponseEntity(ex, request.getRequestURI());
@@ -54,23 +41,23 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<?> handleExpiredJwtException(HttpServletRequest request) {
-        return ErrorResponse.toResponseEntity(GlobalErrorCode.JWT_EXPIRED, request.getRequestURI());
+    public ResponseEntity<ErrorResponse> handleExpiredJwtException(HttpServletRequest request) {
+        return ErrorResponse.toResponseEntity(JWT_EXPIRED, request.getRequestURI());
     }
 
     @ExceptionHandler(SignatureException.class)
-    public ResponseEntity<?> handleSignatureException(HttpServletRequest request) {
-        return ErrorResponse.toResponseEntity(GlobalErrorCode.JWT_NOT_VERIFIED, request.getRequestURI());
+    public ResponseEntity<ErrorResponse> handleSignatureException(HttpServletRequest request) {
+        return ErrorResponse.toResponseEntity(JWT_NOT_VERIFIED, request.getRequestURI());
     }
 
     @ExceptionHandler(MalformedJwtException.class)
-    public ResponseEntity<?> handleMalformedJwtException(HttpServletRequest request) {
-        return ErrorResponse.toResponseEntity(GlobalErrorCode.JWT_MALFORMED, request.getRequestURI());
+    public ResponseEntity<ErrorResponse> handleMalformedJwtException(HttpServletRequest request) {
+        return ErrorResponse.toResponseEntity(JWT_MALFORMED, request.getRequestURI());
     }
 
     @ExceptionHandler(UnsupportedJwtException.class)
-    public ResponseEntity<?> handleUnsupportedJwtException(HttpServletRequest request) {
-        return ErrorResponse.toResponseEntity(GlobalErrorCode.JWT_UNSUPPORTED, request.getRequestURI());
+    public ResponseEntity<ErrorResponse> handleUnsupportedJwtException(HttpServletRequest request) {
+        return ErrorResponse.toResponseEntity(JWT_UNSUPPORTED, request.getRequestURI());
     }
 
 }
