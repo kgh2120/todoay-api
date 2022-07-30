1108dca74418d5bd0115e083d8cfc16c3632f821
kyoohyun <kgh2120@naver.com>
2022-07-30 13:23:46 +0900

    FIX : X-AUTH-TOKEN 헤더에 빈 값을 넣어서 발생하는 IllegalArgumentException에 대한 처리 추가
    #87

--- src/main/java/com/todoay/api/global/jwt/JwtExceptionHandlingFilter.java
+++ src/main/java/com/todoay/api/global/jwt/JwtExceptionHandlingFilter.java
@@ -1,8 +1,7 @@
 package com.todoay.api.global.jwt;
 
-import com.todoay.api.global.exception.AbstractApiException;
 import com.todoay.api.global.exception.ErrorResponse;
-import com.todoay.api.global.exception.GlobalErrorCode;
+import com.todoay.api.global.exception.JwtHeaderNotFoundException;
 import io.jsonwebtoken.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,12 +19,12 @@ public class JwtExceptionHandlingFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
-        } catch (JwtException e) {
+        } catch (JwtException | JwtHeaderNotFoundException e) {
             sendError(HttpStatus.UNAUTHORIZED,request,response,e);
         }
     }
 
-    public void sendError(HttpStatus status,HttpServletRequest request, HttpServletResponse response, JwtException e) throws IOException {
+    public void sendError(HttpStatus status,HttpServletRequest request, HttpServletResponse response, Throwable e) throws IOException {
         response.setStatus(status.value());
         response.setContentType("application/json; charset=utf-8");
 
@@ -38,7 +37,8 @@ public class JwtExceptionHandlingFilter extends OncePerRequestFilter {
             errorMsg =  ErrorResponse.toResponseString(JWT_UNSUPPORTED,request.getRequestURI());
          if (e instanceof SignatureException )
              errorMsg =  ErrorResponse.toResponseString(JWT_NOT_VERIFIED,request.getRequestURI());
-
+         if(e instanceof JwtHeaderNotFoundException)
+             errorMsg =  ErrorResponse.toResponseString(JWT_HEADER_NOT_FOUND,request.getRequestURI());
 
 
         response.getWriter().write(errorMsg);
