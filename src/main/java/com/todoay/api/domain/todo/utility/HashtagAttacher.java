22e97bba16aff94b0b531151c216308bf377f44b
kyoohyun <kgh2120@naver.com>
2022-08-16 15:51:29 +0900

    refactor : HashtagAttacher optional 메서드 리팩토링 #121

--- src/main/java/com/todoay/api/domain/todo/utility/HashtagAttacher.java
+++ src/main/java/com/todoay/api/domain/todo/utility/HashtagAttacher.java
@@ -5,8 +5,8 @@ import com.todoay.api.domain.hashtag.entity.Hashtag;
 import com.todoay.api.domain.hashtag.repository.HashtagRepository;
 import com.todoay.api.domain.todo.entity.Todo;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface HashtagAttacher {
 
@@ -17,11 +17,8 @@ public interface HashtagAttacher {
     }
 
     private static List<Hashtag> getHashtagsByHashtagNames(List<HashtagInfoDto> hashtagNames, HashtagRepository hashtagRepository) {
-        List<Hashtag> tags = new ArrayList<>(); // dailyTodo로 전달할 list객체, 연관관계 메서드를 개별 Hashtag로 변경하면
-        hashtagNames.forEach(n -> hashtagRepository.findByName(n.getName())
-                .ifPresentOrElse(tags::add, // 검색 결과가 존재한다면 연관 관계를 맺을 리스트에 추가
-                        () -> tags.add(hashtagRepository.save(new Hashtag(n.getName()))) // 없다면 DB에 저장하고 저장.
-                ));
-        return tags;
+        return hashtagNames.stream().map(n-> hashtagRepository.findByName(n.getName())
+                .orElseGet(() -> hashtagRepository.save(new Hashtag(n.getName())))
+            ).collect(Collectors.toList());
     }
 }
