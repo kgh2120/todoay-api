a8e9fadbdbcf180aae294b49cc39c9d0d942270d
kyoohyun <kgh2120@naver.com>
2022-08-23 18:17:03 +0900

    Fix : SaveRequestDto들 생성자 관련 문제로 어노테이션 추가  #147

--- src/main/java/com/todoay/api/domain/category/dto/CategorySaveRequestDto.java
+++ src/main/java/com/todoay/api/domain/category/dto/CategorySaveRequestDto.java
@@ -2,13 +2,15 @@ package com.todoay.api.domain.category.dto;
 
 import com.todoay.api.global.customValidation.annotation.ValidationCategoryColor;
 import com.todoay.api.global.customValidation.annotation.ValidationCategoryName;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
-@Builder
+@Builder @NoArgsConstructor @AllArgsConstructor
 public class CategorySaveRequestDto {
     @ValidationCategoryName
     private String name;
