72343fda45b79a9c8ea963216c713d4c70a9d17f
kyoohyun <kgh2120@naver.com>
2022-08-26 11:08:11 +0900

    Docs : Swagger 값 추가 #149

--- src/main/java/com/todoay/api/domain/todo/controller/TodoController.java
+++ src/main/java/com/todoay/api/domain/todo/controller/TodoController.java
@@ -226,6 +226,7 @@ public class TodoController {
             summary = "로그인 유저의 DailyTodo를 삭제한다.",
             responses = {
                     @ApiResponse(responseCode = "201"),  // 요청이 수용되어 리소스가 만들어졌을 때
+                    @ApiResponse(responseCode = "401", description = "Access Token 만료", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
                     @ApiResponse(responseCode = "403", description = "Todo가 로그인 유저의 것이 아님", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
                     @ApiResponse(responseCode = "404", description = "해당 id의 리소스가 존재하지 않음", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
             }
@@ -235,7 +236,16 @@ public class TodoController {
         return ResponseEntity.noContent().build();
     }
 
-
+    @Operation(
+            summary = "Todo의 완료 상태를 반대 상태로 바꾼다.",
+            description = "pathvariable로 전달받은 ID를 통해 Todo를 얻고, 완료 상태를 반대로 바꾼다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "전환 성공"),
+                    @ApiResponse(responseCode = "401", description = "Access Token 만료", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "403", description = "Todo가 로그인 유저의 것이 아님", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "해당 id의 리소스가 존재하지 않음", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @PatchMapping("/{id}/switch")
     public ResponseEntity<Void> switchTodoFinishState(@PathVariable("id") Long id) {
         todoService.switchFinishState(id);
