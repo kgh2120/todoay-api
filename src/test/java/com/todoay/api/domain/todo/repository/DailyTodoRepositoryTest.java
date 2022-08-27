f0e78f24e15ece328511d00edab053de614b7eb3
kyoohyun <kgh2120@naver.com>
2022-08-27 15:26:59 +0900

    fix : 리팩토링 과정에서 충돌난 테스트 코드 수정  #151

--- src/test/java/com/todoay/api/domain/todo/repository/DailyTodoRepositoryTest.java
+++ src/test/java/com/todoay/api/domain/todo/repository/DailyTodoRepositoryTest.java
@@ -2,6 +2,8 @@ package com.todoay.api.domain.todo.repository;
 
 import com.todoay.api.domain.auth.entity.Auth;
 import com.todoay.api.domain.auth.repository.AuthRepository;
+import com.todoay.api.domain.category.entity.Category;
+import com.todoay.api.domain.category.repository.CategoryRepository;
 import com.todoay.api.domain.hashtag.entity.Hashtag;
 import com.todoay.api.domain.hashtag.repository.HashtagRepository;
 import com.todoay.api.domain.todo.entity.DailyTodo;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -20,7 +23,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest @Transactional
 class DailyTodoRepositoryTest {
 
     @Autowired DailyTodoRepository repository;
@@ -29,11 +32,20 @@ class DailyTodoRepositoryTest {
 
     @Autowired
     HashtagRepository hashtagRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     Long id;
 
-    @BeforeEach
+    @BeforeEach @Transactional
     void beforeEach() {
+
+        Category ca = Category.builder()
+                .name("cateogry")
+                .color("#123123")
+                .orderIndex(1)
+                .build();
+        categoryRepository.save(ca);
 
         Auth auth = authRepository.findByEmail("test@naver.com").get();
         SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(auth, null, new ArrayList<>()));
@@ -42,17 +54,18 @@ class DailyTodoRepositoryTest {
         DailyTodo todo = DailyTodo.builder()
                 .title("title")
                 .auth(auth)
+                .category(ca)
                 .dailyDate(LocalDate.now()).build();
 
         List<Hashtag> tags = hashtagRepository.findTop5ByNameStartsWith("#태그");
 
         todo.associateWithHashtag(tags);
 
-        System.out.println(todo);
+        System.out.println("[TODO-READY]"+todo);
 
         DailyTodo save = repository.save(todo);
         id = save.getId();
-        System.out.println(save);
+        System.out.println("[TODO-SAVE]"+save);
 
     }
     @AfterEach
@@ -73,8 +86,14 @@ class DailyTodoRepositoryTest {
         assertThat(todos.size()).isSameAs(1);
     }
 
-    @Test
+    @Test @Transactional
     void findById() {
+        System.out.println("[ID]"+id);
+        List<DailyTodo> all = repository.findAll();
+        for (DailyTodo dailyTodo : all) {
+            System.out.println("[ITER]");
+            System.out.println(dailyTodo);
+        }
         DailyTodo dailyTodo = repository.findDailyTodoById(id).get();
         assertThat(dailyTodo.getTitle()).isEqualTo("title");
     }
