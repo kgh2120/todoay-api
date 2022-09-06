d7cc987211e683ebb2befc2d35ab661cfd19932d
kyoohyun <kgh2120@naver.com>
2022-09-06 10:48:53 +0900

    docs : Swagger 관련 내용 추가 #167

--- src/main/java/com/todoay/api/domain/profile/controller/ProfileController.java
+++ src/main/java/com/todoay/api/domain/profile/controller/ProfileController.java
@@ -50,9 +50,12 @@ public class ProfileController {
             description = "Jwt 토큰을 통해 얻은 email 얻은 정보를 dto로 받은 정보로 수정한다. 이때 JWT 토큰으로 인한 오류와 입력 값에 대한 유효성 검사로 인해 오류가 발생할 수 있다.",
             responses = {
                     @ApiResponse(responseCode = "204", description = "성공"),
-                    @ApiResponse(responseCode = "400", description = "입력 값이 유효성 검사를 통과하지 못했습니다.", content = @Content(schema = @Schema(implementation = ValidErrorResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "1. 입력 값이 유효성 검사를 통과하지 못했습니다.\t\n " +
+                            "2. 이미지 파일이 아닌 파일을 업로드 했습니다.\t\n " +
+                            "3. 파일 사이즈 초과", content = @Content(schema = @Schema(implementation = ValidErrorResponse.class))),
                     @ApiResponse(responseCode = "401", description = "AccessToken 만료 ",content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-                    @ApiResponse(responseCode = "403",description = "허락되지 않은 접근",content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+                    @ApiResponse(responseCode = "403",description = "허락되지 않은 접근",content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "500", description = "IO 리소스 문제 발생",content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
             }
     )
     @PutMapping("/profile/my")
