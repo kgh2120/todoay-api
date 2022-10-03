42b1847cb3a066cb784ccfba61580567438c484b
kyoohyun <kgh2120@naver.com>
2022-10-03 21:06:58 +0900

    CHORE : JWT 관련 로그 제거  #195

--- src/main/java/com/todoay/api/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/todoay/api/global/jwt/JwtAuthenticationFilter.java
@@ -1,9 +1,5 @@
 package com.todoay.api.global.jwt;
 
-import com.todoay.api.domain.auth.entity.Auth;
-import com.todoay.api.global.context.LoginAuthContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,7 +16,7 @@ import java.io.IOException;
 // JWT 토큰으로 인증하고 SecurityContextHolder에 추가하는 필터를 설정하는 클래스
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
     private final JwtProvider jwtTokenProvider;
     private final UserDetailsService userDetailsService;
 
@@ -31,28 +27,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String token = jwtTokenProvider.resolveToken(servletRequest);  // (1)
-        LOGGER.info("[doFilterInternal] token 값 추출 완료. token : {}", token);
-
-        LOGGER.info("[doFilterInternal] token 값 유효성 체크 시작");
-        if (token != null && jwtTokenProvider.validateToken(token) != null) {  // (2)
+        String token = jwtTokenProvider.resolveToken(servletRequest);
+        if (token != null && jwtTokenProvider.validateToken(token) != null) {
             Authentication authentication = this.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);  // (3)
-            LOGGER.info("[doFilterInternal] token 값 유효성 체크 완료");
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
-    // 필터에서 인증이 성공했을 때 SecurityContextHolder에 저장할 Authentication을 생성하는 역할
-    // 이걸 구현하는 편한 방법은 UsernamePasswordAuthenticationToken을 사용하는 것
+
     public Authentication getAuthentication(String token) {
-        LOGGER.info("[getAuthentication] 토큰 인증 정보 조회 시작");
         UserDetails userDetails = userDetailsService.loadUserByUsername(jwtTokenProvider.getUserEmail(token));
-        LOGGER.info("[getAuthentication] 토큰 인증 정보 조회 완료, UserDetails UserName : {}", userDetails.getUsername());
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 }
-// (1) JwtTokenProvider를 통해 servletRequest에서 토큰 추출
-// (2) 토큰에 대한 유효성 검사
-// (3) 토큰이 유효하다면 Authentication 객체를 생성해서 SecurityContextHolder에 추가
